e5df027900fa1523b51cf33e45ea88db8e877407
BurNing1993 <786678257@qq.com>
2018-07-29 21:17:20 +0800

    Initial commit

---
+++ client/pages/component/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      'https://placehold.it/820x230',
+      'https://placehold.it/820x230',
+      'https://placehold.it/820x230'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
