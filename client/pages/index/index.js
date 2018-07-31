e6a65ebc05e08142960fb0147353c8eda941361b
Joey <786678257@qq.com>
2018-07-31 09:14:32 +0800

    up

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,95 +1,57 @@
 //index.js
 //获取应用实例
+import showDetail from "../../modules/showDetail";
+import showcDetail from "../../modules/showcDetail";
 const app = getApp()
 
 Page({
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000,
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    items:[
-      {
-        imgSrc:'../../image/11.png',
-        url:'',
-        title:'teeeee',
-        text:'qqqqq'
-      },
-      {
-        imgSrc:'../../image/11.png',
-        url:'',
-        title:'teeeee',
-        text:'qqqqq'
-      },
-      {
-        imgSrc:'../../image/11.png',
-        url:'',
-        title:'teeeee',
-        text:'qqqqq'
-      },
-      {
-        imgSrc:'../../image/11.png',
-        url:'',
-        title:'teeeee',
-        text:'qqqqq'
-      },
-      {
-        imgSrc:'../../image/11.png',
-        url:'',
-        title:'teeeee',
-        text:'qqqqq'
-      }
-    ]
+    index_slides:[],
+    indicator_dots:true,
+    autoplay:true,
+    interval:2000,
+    duration:1000,
+    nav_data:[],
+    index_activity:[],
+    index_block:[],
+    isTap:false,
+    isLoading:false
+  },
+  onLoad(){
+    const index_slides=app.globalData.index_slides,
+          nav_data=app.globalData.nav_data,
+          index_activity=app.globalData.index_activity,
+          index_block=app.globalData.index_block,
+          sectionList=index_block.map((section)=>{
+      return section.section;
+    })
+    console.log(sectionList);
+    this.setData({
+      index_slides,
+      nav_data,
+      index_activity,
+      index_block,
+    });
+    
+  },
+  onShow(e){
+    this.setData({
+      isTap:false
+    });
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  toSearch(e){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      isTap:true
+    });
+    wx.navigateTo({
+      url: "../search/search"
     })
-  }
+  },
+  // showDetail(e){
+  //   const id=e.currentTarget.dataset.pid;
+  //   console.log(id);
+  // }
+  showDetail,
+  showcDetail
 })
